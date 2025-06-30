e19e1a7779b8d3db1e0d36522920222973439536
neajo <souza.vicente.jean@escola.pr.gov.br>
2025-06-30 09:04:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener(&#39;DOMContentLoaded&#39;, function(){
+const aumentaFonteBotao = document.getElementById(&#39;aumentar-fonte&#39;);
+const diminuiFonteBotao = document.getElementById(&#39;diminuir-fonte&#39;);
 
+let tamanhoAtualFonte = 1;
+aumentaFonteBotao.addEventListener(&#39;click&#39;, function(){
+tamanhoAtualFonte += 0.1;
+document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+});
+
+diminuiFonteBotao.addEventListener(&#39;click&#39;, function(){
+tamanhoAtualFonte -= 0.1;
+document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+});
+
+});
